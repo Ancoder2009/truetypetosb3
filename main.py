44a8eae0ce40ec7a799d8c89fb4ab0ad017b6bd2
ancoder <68029104+Ancoder2009@users.noreply.github.com>
2022-01-05 19:28:36 +0000

    changed illegal chars

--- main.py
+++ main.py
@@ -11,7 +11,11 @@ def render(dir : str, path : str, chars : str):
         image = Image.new(mode="RGBA", size=(50, 50), color=None)
         draw = ImageDraw.Draw(image)
         draw.text((0, 0), i, fill="black", font=font)
-        image.save(dir + "/" + i + ".png", format="png")
+        names = {"/": "fw_slash", "\\": "bw_slash", ":": "colon", "*": "astrick", "?": "qmark", "\"": "quotation", "<": "ar", ">": "al", "|": "straight"}
+        if i in "/\:*?\"<>|":
+            image.save(dir + "/" + names[i] + ".png", format="png")
+        else:
+            image.save(dir + "/" + i + ".png", format="png")
 
 chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*()_+`1234567890-={}[];':\"'1234567890"
     
