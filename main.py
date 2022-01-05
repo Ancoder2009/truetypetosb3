4dbb981aecf68b8852647c5d87d4636a550463ff
ancoder <68029104+Ancoder2009@users.noreply.github.com>
2022-01-05 19:35:38 +0000

    Centered text

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def render(dir : str, path : str, chars : str):
     for i in chars:
         image = Image.new(mode="RGBA", size=(50, 50), color=None)
         draw = ImageDraw.Draw(image)
-        draw.text((0, 0), i, fill="black", font=font)
+        draw.text((25, 25), i, fill="black", font=font)
         names = {"/": "fw_slash", "\\": "bw_slash", ":": "colon", "*": "astrick", "?": "qmark", "\"": "quotation", "<": "ar", ">": "al", "|": "straight"}
         if i in "/\:*?\"<>|":
             image.save(dir + "/" + names[i] + ".png", format="png")
@@ -20,3 +20,4 @@ def render(dir : str, path : str, chars : str):
 chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*()_+`1234567890-={}[];':\"'1234567890"
     
 render("OpenSans", "OpenSans.ttf", chars)
+print("Font is rendered!")
