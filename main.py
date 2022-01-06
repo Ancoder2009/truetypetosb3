0570603c6b7cd0ca5abecd4d47a7c1ebb8989a67
ancoder <68029104+Ancoder2009@users.noreply.github.com>
2022-01-06 02:55:37 +0000

    Added Sprite3 file support and a width_dump list with all the widths.

--- main.py
+++ main.py
@@ -4,17 +4,22 @@ import json
 import zipfile
 import io
 import time
+import random
 from hashlib import md5
+
+current_width_dump = []
 
 def render(dir : str, path : str, chars : str):
     """if not os.path.exists(dir):
         os.mkdir(dir)"""
+    width_dump = ""
     if not os.path.exists(path):
         raise FileNotFoundError("File \"" + path + "\"")
     font = ImageFont.truetype(path, size=28)
     letters = dict()
     for i in chars:
         image = Image.new(mode="RGBA", size=(50, 50), color=None)
+        width_dump += str(font.getsize(i)[0]) + "\n"
         draw = ImageDraw.Draw(image)
         draw.text((12.5, 12.5), i, fill="black", font=font)
         #names = {"/": "fw_slash", "\\": "bw_slash", ":": "colon", "*": "astrick", "?": "qmark", "\"": "quotation", "<": "ar", ">": "al", "|": "straight"}
@@ -25,8 +30,40 @@ def render(dir : str, path : str, chars : str):
         buffer = io.BytesIO()
         image.save(buffer, format="png")
         letters[i] = buffer.getvalue()
+    open("width_dump.txt", 'w').write(width_dump)
+    global current_width_dump
+    current_width_dump = width_dump.split("\n")
     return letters
 
+def createSprite(ttf, chars):
+    if not os.path.exists("temp"):
+        os.mkdir("temp")
+    newsprite = {"isStage":False,"name":ttf.split('.')[0],"variables":{},"lists":{},"broadcasts":{},"blocks":{},"comments":{},"currentCostume":0,"costumes":[],"sounds":[],"volume":100,"layerOrder":1,"visible":True,"x":0,"y":0,"size":100,"direction":90,"draggable":False,"rotationStyle":"all around"}
+    costumes = []
+    pngdata = render(None, ttf, chars)
+    newsprite["lists"][chars[random.randint(1, len(chars) - 1)]] = ["width_dump", current_width_dump]
+    for i in pngdata:
+        md5id = md5(pngdata[i]).hexdigest()
+        costumes.append({"assetId":md5id,"name":i,"bitmapResolution":2,"md5ext":md5id + ".png","dataFormat":"png","rotationCenterX":25,"rotationCenterY":25})
+        file = open("temp/" + md5id + ".png", 'w')
+        file = open("temp/" + md5id + ".png", 'wb')
+        file.write(pngdata[i])
+        newsprite["costumes"] = costumes
+    open("temp/sprite.json", 'w').write(json.dumps(newsprite))
+    zip = zipfile.ZipFile("output.sprite3", 'w')
+    for file in os.listdir("temp"):
+        filepath = os.path.join(file)
+        print("Creating Asset: " + filepath)
+        time.sleep(0.002)
+        zip.write("temp/" + filepath)
+
+    for file in os.listdir("temp"):
+        filepath = os.path.join(file)
+        print("Cleaning Up: " + filepath)
+        time.sleep(0.002)
+        os.remove("temp/" + filepath)
+    os.removedirs("temp")
+    
 def inject(sb3, ttf, chars):
     if not os.path.exists("temp"):
         os.mkdir("temp")
@@ -35,6 +72,7 @@ def inject(sb3, ttf, chars):
     newsprite = {"isStage":False,"name":ttf.split('.')[0],"variables":{},"lists":{},"broadcasts":{},"blocks":{},"comments":{},"currentCostume":0,"costumes":[],"sounds":[],"volume":100,"layerOrder":1,"visible":True,"x":0,"y":0,"size":100,"direction":90,"draggable":False,"rotationStyle":"all around"}
     costumes = []
     pngdata = render(None, ttf, chars)
+    newsprite["lists"][chars[random.randint(1, len(chars) - 1)]] = ["width_dump", current_width_dump]
     for i in pngdata:
         md5id = md5(pngdata[i]).hexdigest()
         costumes.append({"assetId":md5id,"name":i,"bitmapResolution":2,"md5ext":md5id + ".png","dataFormat":"png","rotationCenterX":25,"rotationCenterY":25})
@@ -58,9 +96,10 @@ def inject(sb3, ttf, chars):
         os.remove("temp/" + filepath)
     os.removedirs("temp")
 
-chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*()_+`1234567890-={}[];':\"1234567890"
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*()_+`1234567890-={}[];':\"1234567890.,"
     
 #render("OpenSans", "OpenSans.ttf", chars)
 #print("Font is rendered!")
-inject("project_input.sb3", "Karla-Bold.ttf", chars)
+#inject("project_input.sb3", "Karla-Bold.ttf", chars)
+createSprite("Karla-Bold.ttf", chars)
 print("Injected font!")
